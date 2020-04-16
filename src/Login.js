780868f58f37a5e1881afcfc0292f57b65dbdaa1
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-16 10:31:58 +0700

    mengisi kode Join.js

--- src/Login.js
+++ src/Login.js
@@ -13,36 +13,36 @@ const Login = () => {
         Auth.setLoggedIn(true);
     };
 
-    return(
+    return (
         <div>
             <h1>Login</h1>
-            <form onSubmit = {e => handleForm(e)}>
-                <input 
-                    value = {email}
-                    onChange = {e => setEmail(e.target.value)}
-                    name = "email"
-                    type = "email"
-                    placeholder = "email"
+            <form onSubmit={e => handleForm(e)}>
+                <input
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    name="email"
+                    type="email"
+                    placeholder="email"
                 />
-                <input 
-                    onChange = {e => setPassword(e.target.value)}
-                    name = "password"
-                    value = {password}
-                    type = "password"
-                    placeholder = "password"
+                <input
+                    onChange={e => setPassword(e.target.value)}
+                    name="password"
+                    value={password}
+                    type="password"
+                    placeholder="password"
                 />
                 <hr />
                 <button class="googleBtn" type="button">
-                    <img 
-                        src = "https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-                        alt = "logo"
+                    <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                        alt="logo"
                     />
                     Login With Google
                 </button>
                 <button type="submit">Login</button>
                 <span>{error}</span>
             </form>
-        </div>   
+        </div>
     );
 };
 
