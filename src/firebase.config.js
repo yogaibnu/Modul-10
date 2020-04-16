5722dbc5cea0b8d80dfec54837dd26ff0e54d560
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-16 11:32:08 +0700

    add firebase.config.js

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBbaphmHgUfRCt2IwUjuLadfrctFM_Gi8U",
+    authDomain: "fir-reactjs-bda90.firebaseapp.com",
+    databaseURL: "https://fir-reactjs-bda90.firebaseio.com",
+    projectId: "fir-reactjs-bda90",
+    storageBucket: "fir-reactjs-bda90.appspot.com",
+    messagingSenderId: "66735037199",
+    appId: "1:66735037199:web:5b301c93843a2b5fd23c7a"
+};
