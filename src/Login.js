e179b4104eed4f33a524f8dba54a6331986b5b32
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-16 12:34:17 +0700

    edit and import firebase pada Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from "react";
 import { AuthContext } from "./index";
+import * as firebase from "firebase";
 
 const Login = () => {
     const [email, setEmail] = useState("");
@@ -9,8 +10,17 @@ const Login = () => {
     const Auth = useContext(AuthContext);
     const handleForm = e => {
         e.preventDefault();
-        console.log(Auth);
-        Auth.setLoggedIn(true);
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(res => {
+                if (res.user) Auth.setLoggedIn(true);
+            })
+            .catch(e => {
+                setErrors(e.message);
+            });
+        // console.log(Auth);
+        // Auth.setLoggedIn(true);
     };
 
     return (
